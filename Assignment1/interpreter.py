30a4ab0ce2dc8cb0d5c674215dec20f20a906b55
NightFury13 <develop13mohit@gmail.com>
2015-08-26 22:32:43 +0530

    making more understandable views

--- Assignment1/interpreter.py
+++ Assignment1/interpreter.py
@@ -1,15 +1,36 @@
 import os
 import sys
+import subprocess
 
 if __name__ == '__main__':
     asm_32 = sys.argv[1]
-
+    
     content_32bit = []
     
     with open(asm_32, 'r') as f32:
         for line in f32.readlines():
             content_32bit.append(line.strip())
+   
+    print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    print ">>>>32bit ASM code :"
+    for line in content_32bit:
+        print line
+
+    asm_32_out = asm_32.split('.')[0]+'.o'
     
+    command1 = 'nasm -f elf32 '+asm_32 
+    command2 = 'gcc -m32 '+asm_32_out+' -o 32bit_run'
+    command3 = os.getcwd()+'/32bit_run'
+    process = subprocess.Popen(command1, shell=True, stdout=subprocess.PIPE)
+    process.communicate()
+    process = subprocess.Popen(command2, shell=True, stdout=subprocess.PIPE)
+    process.communicate()
+    process = subprocess.Popen(command3, shell=True, stdout=subprocess.PIPE)
+    
+    print ">>>>32bit executable's output :"
+    print process.communicate()[0]
+    print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+
     content_64bit = []
     for line in content_32bit:
         if len(line.split())>1 and line.split()[0] == 'section':
@@ -37,5 +58,22 @@ if __name__ == '__main__':
     with open('64bit_'+asm_32, 'w') as f64:
         for line in content_64bit:
             f64.write(line+'\n')
+    
+    print ">>>>64bit ASM code :"
+    for line in content_64bit:
+        print line
 
+    asm_64_out = '64bit_'+asm_32_out
 
+    command1 = 'nasm -f elf64 64bit_'+asm_32 
+    command2 = 'ld '+asm_64_out+' -o 64bit_run'
+    command3 = os.getcwd()+'/64bit_run'
+    process = subprocess.Popen(command1, shell=True, stdout=subprocess.PIPE)
+    process.communicate()
+    process = subprocess.Popen(command2, shell=True, stdout=subprocess.PIPE)
+    process.communicate()
+    process = subprocess.Popen(command3, shell=True, stdout=subprocess.PIPE)
+    
+    print ">>>>64bit executable's output :"
+    print process.communicate()[0]
+    print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
